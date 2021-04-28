99102e98eec55859a0b06ec3a919a28163af69b8
Shivam <devshivam1996@gmail.com>
2021-04-28 22:29:51 +0530

    Implemented the Index ejs page

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 require('dotenv').config({path: '.env'});
 
 const app = express();
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('index'));
 
 app.listen(process.env.PORT, () => {
   console.log(`\nServer is listening on Port ${process.env.PORT}`);
