f6ec0fdd5bfb487045e01cbcb649ea52a16c4376
Shivam <devshivam1996@gmail.com>
2021-05-02 00:12:00 +0530

    Calling renamed route files in app js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ require('dotenv').config({path: '.env'});
 const app = express();
 app.set('view engine', 'ejs');
 
-app.use('/', require('./routes/api'));
-app.use('/users', require('./routes/userAuth'));
+app.use('/', require('./routes/baseRoute'));
+app.use('/users', require('./routes/socialAuthRoute'));
 
 app.listen(process.env.PORT, () => {
   console.log(`\nServer is listening on Port ${process.env.PORT}`);
